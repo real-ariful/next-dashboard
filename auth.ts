0721ad7f51da6235cdecdf806c50c14520c2a8a8
ariful@beleaf.co.id <ariful@beleaf.co.id>
2024-12-12 09:36:03 +0600

    UPDATES

--- auth.ts
+++ auth.ts
@@ -17,7 +17,7 @@ async function getUser(email: string): Promise<User | undefined> {
       console.error('Failed to fetch user:', error);
       throw new Error('Failed to fetch user.');
     }
-  }
+}
 
   
 export const { auth, signIn, signOut } = NextAuth({
@@ -34,10 +34,10 @@ export const { auth, signIn, signOut } = NextAuth({
             const user = await getUser(email);
             if (!user) return null;
             const passwordsMatch = await bcrypt.compare(password, user.password);
-
+            console.log('passwordsMatch', passwordsMatch);
             if (passwordsMatch) return user;
           }
-          
+
           console.log('Invalid credentials');
           return null;
         },
